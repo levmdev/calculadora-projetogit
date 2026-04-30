388072fbf791f9ddf347b003fb7ad8912ebcbeeb
neto <uoct50@gmail.com>
2026-04-30 10:36:10 -0300

    (feat): show chars in screen

---
+++ js/calculator.js
@@ -0,0 +1,32 @@
+let displayField = document.querySelector('#current-operand');
+let calculousArray = [];
+
+let numbers = document.querySelectorAll('.btn-number')
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        calculousArray.push(number)
+        let attr = number.getAttributeNames();
+        fillScreen(number.textContent, attr[1])
+    })
+})
+
+let operators = document.querySelectorAll('.btn-operator')
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if(calculousArray.length > 0 && calculousArray[calculousArray.length - 1].getAttribute('data-number') == undefined) return;
+        calculousArray.push(operator)
+        let attr = operator.getAttributeNames();
+        fillScreen(operator.textContent, attr[1]);
+    })
+})
+
+const fillScreen = (input, attr) => {
+    if(attr == 'data-number' && calculousArray.length == 1) {
+        displayField.textContent = input
+        return;
+    } else if(attr == 'data-number' && calculousArray.length != 1) {
+        displayField.textContent += input
+        return;
+    }
+    displayField.textContent += input;
+}
